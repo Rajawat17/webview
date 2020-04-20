9ee646e854857a9498ea9156df90eef79308a4fc
possible3656 <sj94611@gmail.com>
2020-04-20 17:42:28 +0530

    updated now everything work's

--- app/src/main/java/com/example/webview2/MainActivity.java
+++ app/src/main/java/com/example/webview2/MainActivity.java
@@ -8,86 +8,66 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    WebView wv;  //https://seerviks.000webhostapp.com/
 
-    String url;
+    // variables here
 
-    {
-        url = "https://seerviks.000webhostapp.com/";
-    }
+    // for webview
+    WebView webView ;
+
+    // for progressbar
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        wv=(WebView)findViewById(R.id.webview);
-        WebSettings webSettings = wv.getSettings();
-        wv.getSettings().setLoadWithOverviewMode(true);
-        wv.getSettings().setUseWideViewPort(true);
-        wv.getSettings().setBuiltInZoomControls(true);
-        wv.addJavascriptInterface(new WebAppInterface(this), "Android");
-        wv.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        wv.getSettings().setPluginState(WebSettings.PluginState.ON);
-        wv.setWebViewClient(new myWebClient());
+        webView=findViewById(R.id.webview);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
 
-        wv.loadUrl(url);
+
+        getWebSite();
 
     }
-    public class myWebClient extends WebViewClient {
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            // TODO Auto-generated method stub
-            super.onPageStarted(view, url, favicon);
-        }
 
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            // TODO Auto-generated method stub
-            if(Uri.parse(url).getHost().equals(url)){
-                return false;
+    private void getWebSite() {
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
             }
-            startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse(url)));
 
-           // view.loadUrl(url);
-            return true;
-
-        }
-    }
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(event.getAction()==KeyEvent.ACTION_DOWN){
-            switch (keyCode){
-                case KeyEvent.KEYCODE_BACK:
-                    if(wv.canGoBack()){
-                        wv.goBack();
-                    }
-                    else
-                    {
-                        finish();
-                    }
-                    return true;
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
             }
-        }
-        return super.onKeyDown(keyCode,event);
+        });
+        // place any url
+        webView.loadUrl("https://roboclubrtu.com/");
     }
-    public class WebAppInterface {
-        Context mContext;
 
-        /** Instantiate the interface and set the context */
-        WebAppInterface(Context c) {
-            mContext = c;
-        }
-
-        /** Show a toast from the web page */
-        @JavascriptInterface
-        public void showToast(String toast) {
-            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
         }
     }
+
 }
